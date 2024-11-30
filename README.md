39206fb206ee181e8bf75650214ece4e384f471f
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-11-30 20:30:50 +0530

    Implement driver for scarlet

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A custom Compiler in C++. The goal is to learn more about compilers and implement a blazing fast 
 deep learning framework on top of it. 
+
+# Roadmap
+
+1. Implement a compiler driver which has custom lexer, parser, assembly generation pass and code emission pass. Since we are not concerned with linking and preprocessing for now, we use their gcc implementations. 
---
+++ driver/driver.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdlib>
+#include <format>
+#include <vector>
+
+// extract out the name of the file
+// possibilities : ./<path>/<filename>.<ext> or <filename>.<ext>
+#include <filesystem>
+
+bool isFlagValid(const std::string& flag) {
+    return flag == "lex" || flag == "parse" || flag == "codegen";
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        std::cerr << "[USAGE]: " << argv[0] << " <filepath>" << " <flags>" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::string> flags;
+    flags.reserve(argc - 2);
+
+    for(int i = 2; i < argc; i++) {
+        std::string flagArg = std::string(argv[i]);
+        std::string flag = flagArg.substr(2);
+        if (flagArg.substr(0, 2) != "--" or !isFlagValid(flag)) {
+            std::cerr << std::format("[ERROR]: Invalid flag {}", argv[i]) << std::endl;
+            return 1;
+        } else {
+            flags.emplace_back(flag);
+        }
+    }
+
+    std::filesystem::path path(argv[1]);
+    std::string file_name = path.stem().string();
+
+    if(path.extension().string() != ".sc") {
+        std::cerr << "[ERROR]: file format not recognized; It should have an sc extension" << std::endl;
+        return 1;
+    }
+
+    int result = system(std::format("gcc -x c++ -E -P {} -o {}.scp", argv[1], file_name).c_str());
+
+    if (result != 0) {
+        std::cerr << "[ERROR]: Preprocessing failed" << std::endl;
+        return 1;
+    }
+
+    // STUB: convert the preprocessed file to assembly file
+    result = system(std::format("gcc -x c++ -S {}.scp -o {}.s", file_name, file_name).c_str());
+
+    if (result != 0) {
+        std::cerr << "[ERROR]: Compilation failed" << std::endl;
+        return 1;
+    }
+
+    // delete the intermediate preprocessed file
+    result = system(std::format("rm {}.scp", file_name).c_str());
+
+    if (result != 0) {
+        std::cerr << "[ERROR]: Unable to delete the intermediate preprocessed file" << std::endl;
+        return 1;
+    }
+
+    // convert the assembly file to object file
+    result = system(std::format("gcc {}.s -o {}", file_name, file_name).c_str());
+
+    if (result != 0) {
+        std::cerr << "[ERROR]: Failed to generate the executable" << std::endl;
+        return 1;
+    }
+
+    // delete the intermediate assembly file
+    result = system(std::format("rm {}.s", file_name).c_str());
+
+    if (result != 0) {
+        std::cerr << "[ERROR]: Unable to delete the intermediate assembly file" << std::endl;
+        return 1;
+    }
+
+    std::cout << "[LOG]: Scarlet ran Successfully" << std::endl;
+
+    return 0;
+}
