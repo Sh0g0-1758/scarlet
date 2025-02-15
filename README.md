bf7d6697bfbc9c8f10b1d76e99d2f734144d5668
Shourya Goel <shouryagoel10000@gmail.com>
2025-02-15 10:31:18 +0530

    [Scarlet][Core] feat: add support for compound statements (#53)
    * init: ast changes
    
    * done with parser
    
    * pretty print parser
    
    * implement semantic analysis for compound statements
    
    * feat: add support for compound statements
    
    * update docs

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ Scarlet currently supports the following architectures:
 - [x] Add support for increment/decrement operator
 - [x] Add support for compound operators (+=,-=,*=,/=,<<=,>>=,%=)
 - [x] Add support for goto statement
-- [ ] Add support for compound statements
+- [x] Add support for compound statements
 - [ ] Add support for Loops
 - [ ] Add support for Functions
 - [ ] Add support for static keyword and global scope
--- parser/parser.hh
+++ parser/parser.hh
@@ -15,17 +15,18 @@ private:
   bool success = true;
   std::vector<std::string> error_messages;
   int symbol_counter = 0;
-  std::map<std::string, std::string> symbol_table;
   std::map<std::string, bool> goto_labels;
   ast::AST_Program_Node program;
   std::shared_ptr<ast::AST_Function_Node>
   parse_function(std::vector<token::Token> &tokens);
+  void parse_block(std::vector<token::Token> &tokens,
+                   std::shared_ptr<ast::AST_Block_Node> block);
   void parse_block_item(std::vector<token::Token> &tokens,
-                        std::shared_ptr<ast::AST_Function_Node> function);
+                        std::shared_ptr<ast::AST_Block_Node> function);
   void parse_declaration(std::vector<token::Token> &tokens,
-                         std::shared_ptr<ast::AST_Function_Node> function);
+                         std::shared_ptr<ast::AST_Block_Node> function);
   void parse_statement(std::vector<token::Token> &tokens,
-                       std::shared_ptr<ast::AST_Function_Node> function);
+                       std::shared_ptr<ast::AST_Block_Node> function);
   void parse_factor(std::vector<token::Token> &tokens,
                     std::shared_ptr<ast::AST_factor_Node> factor);
   void parse_exp(std::vector<token::Token> &tokens,
@@ -42,12 +43,20 @@ private:
   void eof_error(token::Token token);
   void pretty_print_exp(std::shared_ptr<ast::AST_exp_Node> exp);
   void pretty_print_factor(std::shared_ptr<ast::AST_factor_Node> factor);
+  void pretty_print_block(std::shared_ptr<ast::AST_Block_Node> block);
   std::string get_temp_name(std::string &name) {
     std::string tmp = name + "." + std::to_string(symbol_counter);
     symbol_counter++;
     return tmp;
   }
-  void analyze_exp(std::shared_ptr<ast::AST_exp_Node> exp);
+  void
+  analyze_exp(std::shared_ptr<ast::AST_exp_Node> exp,
+              std::map<std::pair<std::string, int>, std::string> &symbol_table,
+              int indx);
+  void analyze_block(
+      std::shared_ptr<ast::AST_Block_Node> block,
+      std::map<std::pair<std::string, int>, std::string> &symbol_table,
+      int indx);
 
 public:
   void parse_program(std::vector<token::Token> tokens);
