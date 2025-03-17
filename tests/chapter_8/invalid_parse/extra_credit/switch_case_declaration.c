9f6a74bfdf5221151f5715691930667a5225580b
anant <anantjn92004@gmail.com>
2025-03-18 00:52:18 +0530

    changing the test so that it passes

--- tests/chapter_8/invalid_parse/extra_credit/switch_case_declaration.c
+++ tests/chapter_8/invalid_parse/extra_credit/switch_case_declaration.c
@@ -1,8 +1,9 @@
 int main(void) {
-    switch(3) {
-        case 3:
-            int i = 0;
-            return i;
-    }
-    return 0;
+  switch (3) {
+  case 3:
+  case 3:
+    int i = 0;
+    return i;
+  }
+  return 0;
 }
