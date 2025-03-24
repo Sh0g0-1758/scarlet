dfe4f7e411ed29439fd8c1a6a8cd7fa4483f0810
Shourya Goel <shouryagoel10000@gmail.com>
2025-03-24 23:24:28 +0530

    Update pretty_print_block.cc

--- parser/pretty_print/pretty_print_block.cc
+++ parser/pretty_print/pretty_print_block.cc
@@ -6,8 +6,7 @@ namespace parser {
 void parser::pretty_print_block(std::shared_ptr<ast::AST_Block_Node> block) {
   if (block == nullptr)
     return;
-  std::cout << "\t\t\t"
-            << "Block=(" << std::endl;
+  std::cout << "\t\t\t" << "Block=(" << std::endl;
   for (auto blockItem : block->get_blockItems()) {
     if (blockItem->get_type() == ast::BlockItemType::DECLARATION) {
       pretty_print_declaration(blockItem->get_declaration());
