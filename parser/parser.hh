4acf84db6a1ede14687d3f844184fd626a92cca9
atul-k-6 <atul.kumar060804@gmail.com>
2025-03-27 12:38:39 +0530

    casting done

--- parser/parser.hh
+++ parser/parser.hh
@@ -317,18 +317,37 @@ private:
       return constant::Type::INT;
     case ast::ElemType::LONG:
       return constant::Type::LONG;
+    case ast::ElemType::UINT:
+      return constant::Type::UINT;
+    case ast::ElemType::ULONG:
+      return constant::Type::ULONG;
     default:
       return constant::Type::NONE;
     }
   }
 
+  int getSizeType(ast::ElemType type) {
+    if(type == ast::ElemType::INT || type == ast::ElemType::UINT) 
+      return 4;
+    else if(type == ast::ElemType::LONG || type == ast::ElemType::ULONG)
+      return 8;
+    else
+      return -1;
+  }
+
   ast::ElemType getParentType(ast::ElemType left, ast::ElemType right) {
     if (left == right)
       return left;
-    if (left == ast::ElemType::LONG || right == ast::ElemType::LONG) {
-      return ast::ElemType::LONG;
+    else if(getSizeType(left) == getSizeType(right)) {
+      if(left == ast::ElemType::UINT || left == ast::ElemType::ULONG) 
+        return left;
+      else
+        return right;
     }
-    return ast::ElemType::INT;
+    else if(getSizeType(left) > getSizeType(right)) 
+      return left;
+    else
+      return right;
   }
 
   constant::Constant castConstToVal(constant::Constant c, ast::ElemType type) {
@@ -346,7 +365,13 @@ private:
         ret.set_value({.i = static_cast<int>(c.get_value().i)});
         break;
       case constant::Type::LONG:
-        ret.set_value({.i = static_cast<int>(c.get_value().i)});
+        ret.set_value({.i = static_cast<int>(c.get_value().l)});
+        break;
+      case constant::Type::UINT:
+        ret.set_value({.i = static_cast<int>(c.get_value().ui)});
+        break;
+      case constant::Type::ULONG:
+        ret.set_value({.i = static_cast<int>(c.get_value().ul)});
         break;
       default:
         UNREACHABLE();
@@ -360,6 +385,50 @@ private:
         break;
       case constant::Type::LONG:
         ret.set_value({.l = static_cast<long>(c.get_value().l)});
+        break;
+      case constant::Type::UINT:
+        ret.set_value({.l = static_cast<long>(c.get_value().ui)});
+        break;
+      case constant::Type::ULONG:
+        ret.set_value({.l = static_cast<long>(c.get_value().ul)});
+        break;
+      default:
+        UNREACHABLE();
+      }
+    } break;
+    case ast::ElemType::UINT: {
+      ret.set_type(constant::Type::UINT);
+      switch (c.get_type()) {
+      case constant::Type::INT:
+        ret.set_value({.ui = static_cast<unsigned int>(c.get_value().i)});
+        break;
+      case constant::Type::LONG:
+        ret.set_value({.ui = static_cast<unsigned int>(c.get_value().l)});
+        break;
+      case constant::Type::UINT:
+        ret.set_value({.ui = static_cast<unsigned int>(c.get_value().ui)});
+        break;
+      case constant::Type::ULONG:
+        ret.set_value({.ui = static_cast<unsigned int>(c.get_value().ul)});
+        break;
+      default:
+        UNREACHABLE();
+      }
+    } break;
+    case ast::ElemType::ULONG: {
+      ret.set_type(constant::Type::ULONG);
+      switch (c.get_type()) {
+      case constant::Type::INT:
+        ret.set_value({.ul = static_cast<unsigned long>(c.get_value().i)});
+        break;
+      case constant::Type::LONG:
+        ret.set_value({.ul = static_cast<unsigned long>(c.get_value().l)});
+        break;
+      case constant::Type::UINT:
+        ret.set_value({.ul = static_cast<unsigned long>(c.get_value().ui)});
+        break;
+      case constant::Type::ULONG:
+        ret.set_value({.ul = static_cast<unsigned long>(c.get_value().ul)});
         break;
       default:
         UNREACHABLE();
