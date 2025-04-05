ef2fc517d195f56b59493495120d2df420d04d0d
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-04-06 04:49:39 +0530

    scalar type cannot initialize arrays

--- parser/semantic_analysis/analyze_variable_declaration.cc
+++ parser/semantic_analysis/analyze_variable_declaration.cc
@@ -231,21 +231,28 @@ void parser::analyze_local_variable_declaration(
     if (varDecl->get_exp() != nullptr) {
       symbol_table[{var_name, indx}].def = symbolTable::defType::TRUE;
       globalSymbolTable[temp_name].def = symbolTable::defType::TRUE;
-      analyze_exp(varDecl->get_exp(), symbol_table, indx);
-      decay_arr_to_pointer(nullptr, varDecl->get_exp());
-      auto expType = varDecl->get_exp()->get_type();
-      auto expDerivedType = varDecl->get_exp()->get_derived_type();
-      auto [castType, castDerivedType] =
-          ast::getAssignType(varInfo.typeDef[0], varInfo.derivedTypeMap[0],
-                             expType, expDerivedType, varDecl->get_exp());
-      if (castType == ast::ElemType::NONE) {
-        success = false;
-        error_messages.emplace_back("Invalid assignment to variable " +
-                                    var_name);
-      } else {
-        if (castType != expType or castDerivedType != expDerivedType) {
-          add_cast_to_exp(varDecl->get_exp(), castType, castDerivedType);
+      if (varInfo.typeDef[0] != ast::ElemType::DERIVED) {
+        analyze_exp(varDecl->get_exp(), symbol_table, indx);
+        decay_arr_to_pointer(nullptr, varDecl->get_exp());
+        auto expType = varDecl->get_exp()->get_type();
+        auto expDerivedType = varDecl->get_exp()->get_derived_type();
+        auto [castType, castDerivedType] =
+            ast::getAssignType(varInfo.typeDef[0], varInfo.derivedTypeMap[0],
+                               expType, expDerivedType, varDecl->get_exp());
+        if (castType == ast::ElemType::NONE) {
+          success = false;
+          error_messages.emplace_back("Invalid assignment to variable " +
+                                      var_name);
+        } else {
+          if (castType != expType or castDerivedType != expDerivedType) {
+            add_cast_to_exp(varDecl->get_exp(), castType, castDerivedType);
+          }
         }
+      } else {
+        success = false;
+        error_messages.emplace_back(
+            "Invalid assignment to variable " + var_name +
+            ", need an initializer list to initialize arrays");
       }
     } else if (varDecl->get_initializer() != nullptr) {
       symbol_table[{var_name, indx}].def = symbolTable::defType::TRUE;
