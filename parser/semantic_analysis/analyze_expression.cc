392eed0ac39354da56016b42168370d617bda754
4m4n-x-b4w4ne <amanmbawane@gmail.com>
2025-04-10 17:47:55 +0530

    fm

--- parser/semantic_analysis/analyze_expression.cc
+++ parser/semantic_analysis/analyze_expression.cc
@@ -417,6 +417,7 @@ void parser::assign_type_to_factor(
     } else if(factor->get_unop_node()->get_op() == unop::UNOP::NEGATE || factor->get_unop_node()->get_op() == unop::UNOP::COMPLEMENT){
       if (factor->get_type() == ast::ElemType::CHAR) {
         factor->set_type(ast::ElemType::INT);
+      }
     } else {
       factor->set_type(factor->get_child()->get_type());
       factor->set_derived_type(factor->get_child()->get_derived_type());
